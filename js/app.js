eaea8fc2da1ba0997627deafc04ba8150c5c8c6a
ankita31 <er.ankita31@gmail.com>
2013-04-17 11:02:18 +0530

    newform

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var dataset;
 var DataType;
-var createStatement = 'CREATE TABLE IF NOT EXISTS ankita(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT, gender TEXT )';
-var insertStatement = 'INSERT INTO ankita(name, age,gender) VALUES (?,?,?)';
-var updateStatement = 'UPDATE ankita SET name = ?, age = ? gender = ? WHERE id=?';
+var createStatement = 'CREATE TABLE IF NOT EXISTS ankita(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, empid TEXT, email TEXT, age TEXT, gender TEXT , salary TEXT, joiningdate TEXT,image BLOB)';
+var insertStatement = 'INSERT INTO ankita(name, empid,email,age,gender,salary,joiningdate,image) VALUES (?,?,?,?,?,?,?,?)';
+var updateStatement = 'UPDATE ankita SET name = ?, empid= ?,email= ?,age = ?, gender = ?,  salary =?,joiningdate=?,image=?  WHERE id=?';
 var deleteStatement = 'DELETE FROM ankita WHERE id=?';
 var dropStatement = 'DROP TABLE ankita';
 var sort='SELECT * FROM ankita ORDER BY name ASC ';
@@ -14,8 +14,20 @@ var version = '1.0';
 var displayName = 'My Test Database Example';
 var maxSizeInBytes = 65536;
 db = openDatabase(shortName, version, displayName, maxSizeInBytes);
+var inverse = false;
+/*
+$(document).ready(function() 
+    { 
+        $("#tblSearch").tablesorter(); 
+    } 
+); 
 
-
+$(document).ready(function() 
+    { 
+        $("#tblSearch").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+    } 
+); 
+*/
 
 function createTable() {
        db.transaction(function(tx) {
@@ -26,10 +38,18 @@ function createTable() {
 
 function insertRecord() {
     var name = $('#name').val();
-    var age = $('#age').val();
+     var empid = $('#empid').val();
+ var email = $('#email').val();
+     var age = $('#age').val();
     var gender = $('input:radio[name=gender]:checked').val();
+console.log("gender")   
+ var salary = $('#salary').val();
+      var joiningdate = $('#joiningdate').val();
+      var image =$('.images').attr("src");
+    
+      console.log("image")
     db.transaction(function(tx) {
-        tx.executeSql(insertStatement, [name, age,gender], loadAndReset, handleErrors);
+        tx.executeSql(insertStatement, [name, empid,email,age,gender,salary,joiningdate,image], loadAndReset, handleErrors);
     });
 }
 
@@ -44,13 +64,17 @@ function deleteRecord(id) {
 
 function updateRecord() {
     var name = $('#name').val();
+    var empid = $('#empid').val();
+    var email = $('#email').val();
     var age = $('#age').val();
-    var gender = $('input:radio[name=gender]').val();
-      
+    var gender = $('input:radio[name=gender]:checked').val();
+     console.log(gender);
+      var salary = $('#salary').val();
+      var joiningdate = $('#joiningdate').val();
     var id = $('#row_id').data('id');
     
     db.transaction(function(tx) {
-        tx.executeSql(updateStatement, [name, age,gender,id], loadAndReset, handleErrors);
+        tx.executeSql(updateStatement, [name, empid,email,age,gender,salary,joiningdate,id], loadAndReset, handleErrors);
         
     });
 }
@@ -69,16 +93,26 @@ function loadRecord(i) {
     
     var item = dataset.item(i);
     $('#name').val(item.name);
+     $('#empid').val(item['empid']);
+     $('#email').val(item.email);
     $('#age').val(item['age']);
-    var gender = $('input:radio[name=gender]').val();
+        var gender = $('input:radio[name=gender]').val();
+    $('#salary').val(item['salary']);
+     $('.images').attr("src");
+    $('#joiningdate').val(item['joiningdate']);
         $('#row_id').data('id',item['id']);
 }
 
 function resetForm() {
     $('#name').val('');
+    $('#empid').val('');
+    $('#email').val('');
     $('#age').val('');
         $('#id').val('');
         $('input:radio[name=gender]').val('');
+        $('#salary').val('');
+        $('#joiningdate').val('');
+       //$('#photo').item['image'];
 
 }
 
@@ -127,11 +161,17 @@ function renderRecords(transaction, results) {
         html+= '  <caption>Row inserted</caption>';
         html+='  <thead>';
         html+='    <tr>';
-        html+= '      <th  class="span1" ><a href="#" id="asc"></a>Id</th>';
-        html+= '      <th class="span1"><a href="#" class="accending">name</a></th>';
-        html+='      <th class="span2"><a href="#" id="age    ">age</a></th>';
-       html+= '      <th  class="span1" ><a href="#" id="asc"></a>Gender</th>';
-        html+= '      <th class="span1">Actions</td>';
+        html+= '      <th  class="span1" >Id</th>';
+        html+= '      <th class="sort-alpha">name</th>';
+         html+= '      <th class="sort-alpha">empid</th>';
+          html+= '      <th class="sort-alpha">email</th>';
+        html+='      <th class="sort-alpha">age</th>';
+       html+= '      <th  class="sort-alpha" >Gender</th>';
+        html+= '      <th class="sort-alpha">salary</th>';
+         html+= '      <th class="sort-alpha">Date of Joining</th>';
+          html+= '      <th class="sort-alpha">Image</th>';
+
+        html+= '      <th class="sort-alpha">Actions</td>';
         html +='    </tr>';
         html += '  </thead>';
 
@@ -139,12 +179,17 @@ function renderRecords(transaction, results) {
 
         for (var i = 0, item = null; i < dataset.length; i++) {
             item = dataset.item(i);
-
             html +='    <tr>';
             html += '      <td>' + item['id'] + '</td>';
+            
             html += '      <td>' + item['name'] + '</td>';
+             html += '      <td>' + item['empid'] + '</td>';
+              html += '      <td>' + item['email'] + '</td>';
             html += '      <td>' + item['age'] + '</td>';
             html += '      <td>' + item['gender'] + '</td>';
+             html += '      <td>' + item['salary'] + '</td>';
+              html += '      <td>' + item['joiningdate'] + '</td>';
+               html += '      <td>' + item['image'] + '</td>';
             html += '      <td>';
             html += '        <a href="#" onclick="loadRecord(' + i + ')">edit</a></li><br/>';
             html += '        <a href="#" onclick="deleteRecord('+item['id']+')">delete</a></li>';                                      
@@ -200,33 +245,41 @@ var rows = $("#tblSearch tr").length;
 alert('Total Rows: '+rows);
 });
 
+
+	
+	
 	$(document).on('click', '#tblSearch thead th', function() {
-		var table = $('table');
-
-		$('#tblSearch thead th').each(function() {
-
-			var th = $(this), thIndex = th.index(), inverse = false;
-			table.find('td').filter(function() {
-
-				return $(this).index() === thIndex;
-
-			}).sortElements(function(a, b) {
-
-				return $.text([a]) > $.text([b]) ? inverse ? -1 : 1 : inverse ? 1 : -1;
-
-			}, function() {
-
-				// parentNode is the element we want to move
-				return this.parentNode;
-
-			});
-
-			inverse = !inverse;
-
-		});
-
-	}); 
-
+				var table = $('table');
+				
+				
+					var th = $(this), thIndex = th.index();
+					table.find('td').filter(function() {
+						console.log(thIndex);
+		
+						return $(this).index() === thIndex;
+		
+					}).sortElements(function(a, b) {
+						var textA = $.text([a]).toLowerCase();
+						var textB = $.text([b]).toLowerCase();
+		
+						return textA > textB ? inverse ? -1 : 1 : inverse ? 1 : -1;
+		
+					}, function() {
+		
+						// parentNode is the element we want to move
+						return this.parentNode;
+		
+					});
+		
+					inverse = !inverse;
+		
+		
+			}); 
+	
+		
+	
+	
+	
 // 
 // $(document).on('click','#asc', function(e) {
     // e.preventDefault();
@@ -244,94 +297,4 @@ alert('Total Rows: '+rows);
  
  
 
-   /*
- $('#tblSearch #title').click(function(e) {
    
-     var rows = $('#tblSearch  tr td').get();
-     
-     
-     rows.sort(function(a, b) {
-     
-       var A = $(a).children('td').eq(0).text().toUpperCase();
-       var B = $(b).children('td').eq(0).text().toUpperCase();
-       
-       if(A < B) {
-         return -1;
-       }
-       
-       if(A > B) {
-         return 1;
-       }
-       
-       return 0;     
-     
-     
-     });
-     
-     
-     
-     
-     
-     
-     $.each(rows, function(index, row) {
-     
-       $('#tblSearch').children('th').append(row);
-       
-           
-     
-     
-     });
-   
-   
-     e.preventDefault();
-   
-   });
-   
-   
-   $('#tblSearch #director').click(function(e) {
-   
-     var rows = $('#tblSearch tbody  tr').get();
-     
-     
-     rows.sort(function(a, b) {
-     
-       var A = $(a).children('td').eq(1).text().toUpperCase();
-       var B = $(b).children('td').eq(1).text().toUpperCase();
-       
-       if(A < B) {
-         return -1;
-       }
-       
-       if(A > B) {
-         return 1;
-       }
-       
-       return 0;     
-     
-     
-     });
-     
-     
-     
-     
-     
-     
-     $.each(rows, function(index, row) {
-     
-       $('#tblSearch').children('tbody').append(row);
-       
-           
-     
-     
-     });
-   
-   
-     e.preventDefault();
-   
-   });
- 
- */
-
- 
-// });
- 
